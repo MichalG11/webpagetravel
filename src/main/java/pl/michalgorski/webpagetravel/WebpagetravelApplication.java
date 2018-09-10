ae814e72c740b27708a469a2c4759a3f655b797e
MichalG11 <grsk11@wp.pl>
2018-09-10 12:11:09 +0200

    Initial commit

---
+++ src/main/java/pl/michalgorski/webpagetravel/WebpagetravelApplication.java
@@ -0,0 +1,12 @@
+package pl.michalgorski.webpagetravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpagetravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpagetravelApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/michalgorski/webpagetravel/controllers/MainController.java
@@ -0,0 +1,14 @@
+package pl.michalgorski.webpagetravel.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String showPage() {
+        return "index";
+    }
+
+}
